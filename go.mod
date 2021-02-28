89cfe9057c76885969d1ae433c107a5286b0072a
kecci <kecci@machtwatch.co.id>
2021-02-28 13:32:31 +0700

    initiate fasthttp-swagger

---
+++ go.mod
@@ -0,0 +1,11 @@
+module github.com/kecci/fasthttp-swagger
+
+go 1.15
+
+require (
+	github.com/alecthomas/template v0.0.0-20190718012654-fb15b899a751
+	github.com/fasthttp/router v1.3.7
+	github.com/swaggo/http-swagger v1.0.0
+	github.com/swaggo/swag v1.7.0
+	github.com/valyala/fasthttp v1.21.0
+)
---
+++ main.go
@@ -0,0 +1,85 @@
+package main
+
+import (
+	"github.com/fasthttp/router"
+	docs "github.com/kecci/fasthttp-swagger/docs"
+	httpSwagger "github.com/swaggo/http-swagger"
+	"github.com/valyala/fasthttp"
+	"github.com/valyala/fasthttp/fasthttpadaptor"
+)
+
+// @title Swagger Example API
+// @version 1.0
+// @description This is a sample server celler server.
+// @termsOfService http://swagger.io/terms/
+
+// @contact.name API Support
+// @contact.url http://www.swagger.io/support
+// @contact.email support@swagger.io
+
+// @license.name Apache 2.0
+// @license.url http://www.apache.org/licenses/LICENSE-2.0.html
+
+// @host localhost:9090
+// @BasePath /api/v1
+// @query.collection.format multi
+
+// @securityDefinitions.basic BasicAuth
+
+// @securityDefinitions.apikey ApiKeyAuth
+// @in header
+// @name Authorization
+
+// @securitydefinitions.oauth2.application OAuth2Application
+// @tokenUrl https://example.com/oauth/token
+// @scope.write Grants write access
+// @scope.admin Grants read and write access to administrative information
+
+// @securitydefinitions.oauth2.implicit OAuth2Implicit
+// @authorizationurl https://example.com/oauth/authorize
+// @scope.write Grants write access
+// @scope.admin Grants read and write access to administrative information
+
+// @securitydefinitions.oauth2.password OAuth2Password
+// @tokenUrl https://example.com/oauth/token
+// @scope.read Grants read access
+// @scope.write Grants write access
+// @scope.admin Grants read and write access to administrative information
+
+// @securitydefinitions.oauth2.accessCode OAuth2AccessCode
+// @tokenUrl https://example.com/oauth/token
+// @authorizationurl https://example.com/oauth/authorize
+// @scope.admin Grants read and write access to administrative information
+
+// @x-extension-openapi {"example": "value on a json format"}
+func main() {
+	docs.SwaggerInfo.Title = "Fasthttp Swagger"
+	docs.SwaggerInfo.Description = "Fasthttp Swagger"
+	docs.SwaggerInfo.Version = "1.0.0-0"
+	docs.SwaggerInfo.Schemes = []string{"http", "https"}
+	docs.SwaggerInfo.Host = ""
+	docs.SwaggerInfo.BasePath = ""
+
+	r := router.New()
+	r.GET("/docs/{filepath:*}", fasthttpadaptor.NewFastHTTPHandlerFunc(httpSwagger.WrapHandler))
+	r.GET("/hello", Hello())
+
+	if err := fasthttp.ListenAndServe(":9090", r.Handler); err != nil {
+		panic(err)
+	}
+}
+
+// Hello godoc
+// @Summary Show a Hello
+// @Description get hello
+// @Tags hello
+// @Accept json
+// @Produce json
+// @Success 200 {object} string
+// @Router /hello [get]
+func Hello() fasthttp.RequestHandler {
+	return func(ctx *fasthttp.RequestCtx) {
+		ctx.SetStatusCode(200)
+		ctx.SetBody([]byte(`{ "message" : "HELLO" }`))
+	}
+}
